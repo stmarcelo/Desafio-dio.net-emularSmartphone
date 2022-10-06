357a524283f206e7990800cfa97712c7db5c6a9f
stmarcelo <stmarcelo@yahoo.com.br>
2022-10-06 09:57:32 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "11933221100", modelo: "Modelo I2", imei: "0987654321", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "11900112233", modelo: "Modelo A1", imei: "1234567890", memoria: 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Jogo da cobrinha");
---
+++ README.md
@@ -0,0 +1,4 @@
+# DIO - Trilha .NET - Programação orientada a objetos
+
+## Contexto
+Você é responsável por modelar um sistema que trabalha com celulares. Para isso, foi solicitado que você faça uma abstração de um celular e disponibilize maneiras de diferentes marcas e modelos terem seu próprio comportamento, possibilitando um maior reuso de código e usando a orientação a objetos.
